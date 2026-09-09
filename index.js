3bb4112675f004357ab9f7cac7cf626aba404d3f
fazzahk229-collab / Arslan_MD <fazzahk229@gmail.com>
2026-09-10 00:40:03 +0100

    Set up HTTP server for StatusReact Bot

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("StatusReact Bot est démarré !");
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("StatusReact Bot fonctionne !");
+});
+
+server.listen(PORT, () => {
+  console.log(`StatusReact Bot est démarré sur le port ${PORT}`);
+});
